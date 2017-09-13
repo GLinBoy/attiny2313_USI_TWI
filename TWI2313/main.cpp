5032d1156d4397120b514d0027acbd7117c36268
GLinBoy <hojjatabedie@gmail.com>
2017-09-13 21:30:44 +0430

    init project

---
+++ TWI2313/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * TWI2313.cpp
+ *
+ * Created: 9/13/2017 9:28:45 PM
+ * Author : Hojjat Abedie
+ */ 
+
+ #define F_CPU 20000000UL
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
